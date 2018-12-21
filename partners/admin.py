b67db48e087ff4f0d5f1a39f07dccb711a793483
gmdzy2010 <gmdzy2010@126.com>
2018-12-21 15:14:09 +0800

    liy-20181221-新增合作方管理partners(0.8.0)，测试通过

---
+++ partners/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+
+class PartnersAdmin(admin.ModelAdmin):
+    autocomplete_fields = ("bms_user", )
+    fields = (
+        "bms_user", "code", "name", "mode", "region", "materials",
+        "sponsorship", "activities", "note"
+    )
+    list_per_page = 30
+    list_display = (
+        "bms_user", "code", "name", "created_at", "altered_at",
+        "note",
+    )
+    list_display_links = ('bms_user', )
+    list_filter = ("mode", "region")
+    radio_fields = {"mode": admin.HORIZONTAL, "region": admin.HORIZONTAL}
+    save_as_continue = False
+    search_fields = ("bms_user__username", )
+
+
+class PropagandaAdmin(admin.ModelAdmin):
+    autocomplete_fields = ("partner", "bms_user")
+    fields = ("partner", "date", "bms_user",)
+    list_per_page = 30
+    list_display = ("partner", "date", "bms_user",)
+    list_display_links = ('bms_user', )
+    save_as_continue = False
+    search_fields = ("bms_user__username", "partner__name")
--- partners/urls.py
+++ partners/urls.py
@@ -5,6 +5,4 @@ from accounts import views
 
 urlpatterns = [
     # path(r'^$', views.index, name='index'),
-    # path(r'^index/$', views.index, name='index'),
-    # path(r'^register/$', views.register, name='register'),
 ]
