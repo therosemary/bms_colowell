e81dcc3b05033de2022732c5665a63347ce4d742
gmdzy2010 <gmdzy2010@126.com>
2018-12-21 15:20:20 +0800

    liy-20181221-更新accounts的测试(0.1.0)

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,29 +5,21 @@ from accounts.models import *
 class TestAccounts(TestCase):
     
     def setUp(self):
-        print("【用户管理】test start\n")
-    
-    def tearDown(self):
-        print("【用户管理】test done\n")
-
-    def test_can_create_bms_user(self):
-        user = BmsUser.objects.create_user(
+        self.user = BmsUser.objects.create_user(
             username="TDD_TEST_USER", password="TDD_TEST_USER_123456"
         )
-        self.assertTrue(user)
-        
+    
+    def tearDown(self):
+        self.user.delete()
+    
     def test_can_create_dingtalk_info(self):
-        user = BmsUser.objects.get(username="TDD_TEST_USER")
         dingtalk_info = DingtalkInfo.objects.create(
-            userid="fewfd1223123", bms_user=user
+            userid="fewfd1223123", bms_user=self.user
         )
-        print(dingtalk_info)
         self.assertTrue(dingtalk_info)
-
+    
     def test_can_create_wechat_info(self):
-        user = BmsUser.objects.get(username="TDD_TEST_USER")
         wechat_info = WechatInfo.objects.create(
-            openid="fewfd1223123", bms_user=user
+            openid="fewfd1223123", bms_user=self.user
         )
-        print(wechat_info)
         self.assertTrue(wechat_info)
