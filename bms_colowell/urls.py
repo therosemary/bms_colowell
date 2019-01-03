03685e9a258179e520b89fb7e5aef86d95f78741
gmdzy2010 <gmdzy2010@126.com>
2019-01-03 18:44:54 +0800

    liy-20190103-更新admin模板

--- bms_colowell/urls.py
+++ bms_colowell/urls.py
@@ -5,8 +5,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', BMS_admin_site.urls),
     
-    path('cas/', include(("mama_cas.urls", "mama_cas"), namespace="mama_cas")),
-    
     path('accounts/', include(("accounts.urls", "accounts"), namespace="accounts")),
     path('partners/', include(("partners.urls", "partners"), namespace="partners")),
     path('products/', include(("products.urls", "products"), namespace="products")),
