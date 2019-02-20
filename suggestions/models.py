78fce8663e21d2804fb0219686f358608af62eef
gmdzy2010 <gmdzy2010@126.com>
2019-02-20 17:34:37 +0800

    liy-20190220-suggestion mapping功能的model

--- suggestions/models.py
+++ suggestions/models.py
@@ -1,8 +1,9 @@
+import re
 from django.db import models
 from products.models import Products
 
 
-class Suggestions(models.Model):
+class Collections(models.Model):
     FACTOR_CHOICES = (
         (1, "经常"),
         (2, "偶尔"),
@@ -56,3 +57,70 @@ class Suggestions(models.Model):
 
     def __str__(self):
         return '盒子{}'.format(self.product)
+
+
+class Factors(models.Model):
+    code = models.CharField(
+        verbose_name="因子代号", max_length=4, primary_key=True,
+    )
+    name = models.CharField(
+        verbose_name="因子名称", max_length=32,
+    )
+    create_at = models.DateField(
+        verbose_name="创建于", auto_now_add=True,
+    )
+    
+    def __str__(self):
+        return self.name
+
+
+class Choices(models.Model):
+    code = models.CharField(
+        verbose_name="选项代号", max_length=8, primary_key=True
+    )
+    name = models.CharField(
+        verbose_name="选项名称", max_length=64,
+    )
+    create_at = models.DateField(
+        verbose_name="创建于", auto_now_add=True,
+    )
+    factor = models.ForeignKey(
+        Factors, verbose_name="因子", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Suggestions(models.Model):
+    factors = models.ManyToManyField(
+        Factors, verbose_name="关联因子",
+    )
+    code = models.CharField(
+        verbose_name="建议代号", max_length=4, primary_key=True,
+    )
+    name = models.CharField(
+        verbose_name="建议名称", max_length=64, null=True, blank=True,
+    )
+    connections = models.CharField(
+        verbose_name="关联选项代号", max_length=128,
+        help_text="参照可选择的选项输入，需要先选中关联因子保存，这里才可以显示"
+    )
+    expressions = models.TextField(
+        verbose_name="表达形式",
+    )
+    
+    @property
+    def all_choices(self):
+        factors = self.factors.all()
+        choices = {factor.choices_set for factor in factors}
+        return choices
+    
+    @property
+    def expression_set(self):
+        expressions_list = re.match("[【](.*?)[】]", self.expressions)
+        return set(expressions_list.groups())
+    
+    def __str__(self):
+        return self.code
+
