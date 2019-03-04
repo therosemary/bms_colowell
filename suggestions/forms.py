ac0d18e86d24eef1442f32ffbe98b7171a99d3dd
gmdzy2010 <gmdzy2010@126.com>
2019-03-04 11:04:13 +0800

    liy-20190304-更新admin显示

--- suggestions/forms.py
+++ suggestions/forms.py
@@ -20,31 +20,31 @@ class CollectionsForm(forms.ModelForm):
     )
     t01 = forms.CharField(
         label="饮食建议", required=False,
-        widget=forms.Textarea(attrs={'cols': 50, 'rows': 6}),
+        widget=forms.Textarea(attrs={'cols': 100, 'rows': 5}),
     )
     t02 = forms.CharField(
         label="生活方式", required=False,
-        widget=forms.Textarea(attrs={'cols': 50, 'rows': 6}),
+        widget=forms.Textarea(attrs={'cols': 100, 'rows': 5}),
     )
     t03 = forms.CharField(
         label="体育锻炼", required=False,
-        widget=forms.Textarea(attrs={'cols': 50, 'rows': 6}),
+        widget=forms.Textarea(attrs={'cols': 100, 'rows': 5}),
     )
     t04 = forms.CharField(
         label="健康乐观心态", required=False,
-        widget=forms.Textarea(attrs={'cols': 50, 'rows': 6}),
+        widget=forms.Textarea(attrs={'cols': 100, 'rows': 5}),
     )
     t05 = forms.CharField(
         label="定期筛查", required=False,
-        widget=forms.Textarea(attrs={'cols': 50, 'rows': 6}),
+        widget=forms.Textarea(attrs={'cols': 100, 'rows': 5}),
     )
     t06 = forms.CharField(
         label="确诊和治疗", required=False,
-        widget=forms.Textarea(attrs={'cols': 50, 'rows': 6}),
+        widget=forms.Textarea(attrs={'cols': 100, 'rows': 5}),
     )
     t07 = forms.CharField(
         label="肠镜检查准备", required=False,
-        widget=forms.Textarea(attrs={'cols': 50, 'rows': 6}),
+        widget=forms.Textarea(attrs={'cols': 100, 'rows': 5}),
     )
     
     class Meta:
