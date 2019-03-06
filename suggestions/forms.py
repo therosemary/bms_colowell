74d484bd66a31408cc24b9a177263a511eae4341
gmdzy2010 <gmdzy2010@126.com>
2019-03-06 19:37:04 +0800

    liy-20190306-更新suggestions版本限制

--- suggestions/forms.py
+++ suggestions/forms.py
@@ -3,21 +3,21 @@ from suggestions.models import Collections, Choices
 
 
 class CollectionsForm(forms.ModelForm):
-    _f05 = forms.ModelMultipleChoiceField(
-        label="下消化道症状",
-        queryset=Choices.objects.filter(code__contains="f05"),
-        widget=forms.CheckboxSelectMultiple()
-    )
-    _f06 = forms.ModelMultipleChoiceField(
-        label="其它病史",
-        queryset=Choices.objects.filter(code__contains="f06"),
-        widget=forms.CheckboxSelectMultiple()
-    )
-    _f07 = forms.ModelMultipleChoiceField(
-        label="慢性病史",
-        queryset=Choices.objects.filter(code__contains="f07"),
-        widget=forms.CheckboxSelectMultiple()
-    )
+    # _f05 = forms.ModelMultipleChoiceField(
+    #     label="下消化道症状",
+    #     queryset=Choices.objects.filter(code__contains="f05"),
+    #     widget=forms.CheckboxSelectMultiple()
+    # )
+    # _f06 = forms.ModelMultipleChoiceField(
+    #     label="其它病史",
+    #     queryset=Choices.objects.filter(code__contains="f06"),
+    #     widget=forms.CheckboxSelectMultiple()
+    # )
+    # _f07 = forms.ModelMultipleChoiceField(
+    #     label="慢性病史",
+    #     queryset=Choices.objects.filter(code__contains="f07"),
+    #     widget=forms.CheckboxSelectMultiple()
+    # )
     t01 = forms.CharField(
         label="饮食建议", required=False,
         widget=forms.Textarea(attrs={'cols': 100, 'rows': 5}),
